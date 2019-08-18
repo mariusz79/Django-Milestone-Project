78bcafe3bf88ff3c3036b5983a3d01d147a4ccd5
mariusz79 <mpawlowic@gmail.com>
2019-08-18 18:55:16 +0100

    Created logout view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url 
 from django.contrib.auth import views as auth_views
 from accounts.views import registration
+from .views import logout
  
 
 urlpatterns = [
-    url(r'^login/', auth_views.LoginView.as_view(template_name='login.html') , name='user-login' ),
     url(r'^register/', registration, name="registration"),
+    url(r'^login/', auth_views.LoginView.as_view(template_name='login.html') , name='user-login' ),
+    url(r'^logout/', logout, name="logout"),
+    
    
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,14 @@
-from django.shortcuts import render
 from django.shortcuts import render, redirect, reverse 
 from django.contrib import auth, messages
 from django.contrib.auth.models import User 
 from .forms import UserRegistrationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def registration(request):
     """Render the registration page"""
     if request.user.is_authenticated:
-        return redirect(reverse('index'))
+        return redirect(reverse('home'))
 
     if request.method == "POST":
         registration_form = UserRegistrationForm(request.POST)
@@ -29,3 +29,10 @@ def registration(request):
         registration_form = UserRegistrationForm(None)
     return render(request, 'registration.html',
                   {"registration_form": registration_form})
+
+@login_required
+def logout(request):
+    """Log the user out"""
+    auth.logout(request)
+    messages.success(request, "You have successfully been logged out")
+    return redirect(reverse('home'))
