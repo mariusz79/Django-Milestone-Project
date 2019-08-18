315db0a02dab776f245a376aedb18180059aa949
mariusz79 <mpawlowic@gmail.com>
2019-08-18 19:32:18 +0100

    Created Profile model with resized image field, add signals

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django_resized import ResizedImageField
 
 
-    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = ResizedImageField(default='default.jpg', upload_to='profile_pics', size=[250, 250], crop=['middle', 'center'])
+    bugs = models.IntegerField(default=0)
+    features = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+   
--- issuetracker/settings.py
+++ issuetracker/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_forms_bootstrap',
+    'django_resized',
     'home',
     'accounts',
 ]
@@ -131,4 +132,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
- 
+
+DJANGORESIZED_DEFAULT_FORCE_FORMAT = 'JPEG'
+DJANGORESIZED_DEFAULT_FORMAT_EXTENSIONS = {'JPEG': ".jpg"}
