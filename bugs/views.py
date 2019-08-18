11946ef24f767f2b9905ebfba8af4ee9d28f51bb
mariusz79 <mpawlowic@gmail.com>
2019-08-18 22:33:39 +0100

    Add edit_bug and liking_bug views

--- bugs/views.py
+++ bugs/views.py
@@ -28,3 +28,43 @@ def create_bug(request, pk=None):
 
     return render(request, 'bugform.html', {'form': form})
 
+@login_required
+def edit_bug(request, pk=None):
+    bug = get_object_or_404(Bug, pk=pk) if pk else None
+    if request.user == bug.author:
+        if request.method == "POST":
+            form = BugForm(request.POST, instance=bug)
+            if form.is_valid():
+                bug = form.save()
+                return redirect(bug_detail, bug.pk)
+        else:
+            if request.user == bug.author:
+                form = BugForm(instance=bug)
+    else:
+        messages.info(request, "You need to be bug author to edit it")
+        return redirect(bug_detail, bug.pk)
+
+    return render(request, 'bugform.html', {'form': form})
+
+@login_required
+def liking_bug(request, pk):
+    bug = get_object_or_404(Bug, pk=pk)
+    if bug.status != 4:
+        #check if post is liked by user
+        if request.user.is_authenticated():
+            is_liked = BugLike.objects.filter(liked_bug=pk,
+                                        liker_user=request.user).exists()
+        else:
+            is_liked = False
+
+        if not is_liked:
+            bug.likes += 1
+            bug.save()
+            newlike = BugLike.objects.create(liker_user=request.user, liked_bug=bug)
+        else:
+            messages.info(request, "You can like only once")
+            return redirect(bug_detail, bug.pk)
+    else:
+        messages.info(request, "Likes are disabled for closed bugs")
+        return redirect(bug_detail, bug.pk)
+    return redirect(bug_detail, bug.pk)
