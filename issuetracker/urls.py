2499e1b994e183cde8009d40bd7991f7c4fed0c8
mariusz79 <mpawlowic@gmail.com>
2019-08-18 18:16:06 +0100

    Updating urlpatterns in main app to serve files from media and static folders

--- issuetracker/urls.py
+++ issuetracker/urls.py
@@ -17,9 +17,13 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from home import urls as home_urls
 from accounts import urls as accounts_urls
+from django.views.static import serve
+from .settings import MEDIA_ROOT, STATIC_ROOT
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include(home_urls)),
     url(r'^accounts/', include(accounts_urls)),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': STATIC_ROOT}),
 ]
