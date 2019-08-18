14c90a8086bd99d70cf9919242da5fc9cbe3f3b6
mariusz79 <mpawlowic@gmail.com>
2019-08-18 20:35:23 +0100

    Styling paswword reset form template

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^register/', registration, name="registration"),
     url(r'^login/', auth_views.LoginView.as_view(template_name='login.html') , name='user-login' ),
     url(r'^logout/', logout, name="logout"),
+    url(r'^password-reset/', include(url_reset)),
     
    
 ]
