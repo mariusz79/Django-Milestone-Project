dff9a5d7d1ec26d08f883f7019095fb9e7ef76d3
mariusz79 <mpawlowic@gmail.com>
2019-08-18 22:36:47 +0100

    Update urls patterns for bugs

---
+++ bugs/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from .views import get_bugs, bug_detail, create_bug, edit_bug, liking_bug, sort_bugs
+
+
+
+urlpatterns = [
+    url(r'^$', sort_bugs, name='get_bugs'),
+    url(r'^(?P<pk>\d+)/$', bug_detail, name='bug_detail'),
+    url(r'^(?P<pk>\d+)/like$', liking_bug, name='liking_bug'),
+    url(r'^new/$', create_bug, name='new_bug'),
+    url(r'^(?P<pk>\d+)/edit/$', edit_bug, name='edit_bug')
+]
