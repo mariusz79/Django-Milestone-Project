51e0257474b64846c3ab7ae074e1555918aa94f8
mariusz79 <mpawlowic@gmail.com>
2019-08-18 23:07:21 +0100

    Created create and comments forms for Features

---
+++ features/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Feature, FeatureComment
+
+class FeatureForm(forms.ModelForm):
+    class Meta:
+        model = Feature
+        fields = ('title', 'description', 'tag', 'published_date')
+
+
+class FeatureCommentForm(forms.ModelForm):
+    text = forms.CharField(widget=forms.Textarea(attrs={
+        'cols': 200,
+        'rows': 3,
+        'style': 'width: 100%'
+    }))
+
+    class Meta:
+        model = FeatureComment
+        fields = ('email', 'text',)
