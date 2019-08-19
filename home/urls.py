801b3903287005addc8aebe62bd767be1b43daf7
mariusz79 <mpawlowic@gmail.com>
2019-08-19 17:58:02 +0100

    Add stats template

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from .views import index
+from .views import index, stats
 
 
 
 urlpatterns = [
     url(r'^$', index, name='home'),
     url(r'^home/$',index, name='home'),
+    url(r'^home/stats/$',stats, name='stats'),
 ]
