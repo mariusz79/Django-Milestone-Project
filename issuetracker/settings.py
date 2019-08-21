ef849b6e10cd323d23ddff890e046b0dc82caf2e
mariusz79 <mpawlowic@gmail.com>
2019-08-21 11:55:07 +0100

    Changed name of the AWS bucket

--- issuetracker/settings.py
+++ issuetracker/settings.py
@@ -150,7 +150,7 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=94608000'
 }
 
-AWS_STORAGE_BUCKET_NAME = 'mariusz-ecommerce'
+AWS_STORAGE_BUCKET_NAME = 'issuetracker-django'
 AWS_S3_REGION_NAME = 'eu-west-1'
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_SECRET_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
