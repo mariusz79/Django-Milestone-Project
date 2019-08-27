b8daea7ccbad10c9391bd30457c037b8f482fcfb
mariusz79 <mpawlowic@gmail.com>
2019-08-27 17:53:00 +0100

    Sendgrip removed

--- issuetracker/settings.py
+++ issuetracker/settings.py
@@ -180,13 +180,11 @@ DJANGORESIZED_DEFAULT_FORCE_FORMAT = 'JPEG'
 DJANGORESIZED_DEFAULT_FORMAT_EXTENSIONS = {'JPEG': ".jpg"}
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
- 
-
-EMAIL_HOST_USER = os.environ.get['SENDGRID_USERNAME']
-EMAIL_HOST= 'smtp.sendgrid.net'
-EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = os.environ.get['SENDGRID_PASSWORD']
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_PORT = 587
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 AUTHENTICATION_BACKENDS = [
