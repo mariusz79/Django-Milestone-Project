44ac1c74f924ecd527b3407b00677a6a093e1d22
mariusz79 <mpawlowic@gmail.com>
2019-08-27 17:34:27 +0100

    Setting up sendgrid

--- contact/views.py
+++ contact/views.py
@@ -19,7 +19,7 @@ def contact(request):
             try:
                 message_body = 'From:  email: {email} \n subject: {subject} \n message: {message}'.format(email=email, 
             subject=subject, message=message)
-                send_mail(subject, message_body, settings.EMAIL_HOST_USER, [email_address])
+                send_mail(subject, message_body, email, [email_address])
             except BadHeaderError:
                 return HttpResponse('Invalid header found.')
             return redirect('contact_success')
@@ -29,3 +29,4 @@ def contact(request):
 def contact_success(request):
     """display contact_sucess.html if message send"""
     return render(request, "contact_success.html")
+
--- issuetracker/settings.py
+++ issuetracker/settings.py
@@ -180,12 +180,13 @@ DJANGORESIZED_DEFAULT_FORCE_FORMAT = 'JPEG'
 DJANGORESIZED_DEFAULT_FORMAT_EXTENSIONS = {'JPEG': ".jpg"}
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+ 
+
+EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+EMAIL_HOST= 'smtp.sendgrid.net'
 EMAIL_PORT = 587
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 AUTHENTICATION_BACKENDS = [
